85d3431fdb7ffe8f70a9223686c8c387a385ccfb
Nicklas <nicklas.hug@gmail.com>
2019-10-24 10:46:52 +0200

    NEW VERSION!

--- src/cupcarbon/CupCarbonVersion.java
+++ src/cupcarbon/CupCarbonVersion.java
@@ -3,5 +3,5 @@ package cupcarbon;
 public final class CupCarbonVersion {
 	public static final String VERSION = "U-One 4.0";
 	public static final String YEAR = "2019";
-	public static final int UPDATE = 23;
+	public static final int UPDATE = 99;
 }
