e20e12cd0813d21861e40be61410d0ad28407f89
Nicklas <nicklas.hug@gmail.com>
2019-11-01 12:29:01 +0100

    Add exception for when attempting to remove non existing nodecost

---
+++ src/d504/exceptions/AttemptedToRemoveNonExistingNodeCost.java
@@ -0,0 +1,7 @@
+package d504.exceptions;
+
+public class AttemptedToRemoveNonExistingNodeCost extends RuntimeException {
+    public AttemptedToRemoveNonExistingNodeCost(String message) {
+        super(message);
+    }
+}
