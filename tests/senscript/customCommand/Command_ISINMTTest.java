fad0e2c13e134375493c3f95c8119adbce10a76d
Jonas <jasv16@student.aau.dk>
2019-11-05 13:15:02 +0100

    added 2 more tests for isinmt

--- tests/senscript/customCommand/Command_ISINMTTest.java
+++ tests/senscript/customCommand/Command_ISINMTTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class Command_ISINMTTest {
 
     @Test
-    void execute(){
+    void IsInMT1(){
         TestableSensorNode sensor = new TestableSensorNode(2);
         DataPackage message = new DataPackage("12","1","Hello world");
         DataPackage message2 = new DataPackage("10","1","Bye world");
@@ -24,7 +24,38 @@ class Command_ISINMTTest {
         isinmt.execute();
         assertEquals("true",sensor.getVariableValue("$"+output));
 
+    }
 
+    @Test
+    void IsInMT2(){
+        TestableSensorNode sensor = new TestableSensorNode(3);
+        DataPackage message = new DataPackage("12","1","Hello world");
+        DataPackage message2 = new DataPackage("10","3","Bye world");
+        String output = "x";
+        String mtVar = "mt";
+        MessageTable messageTable = new MessageTable();
+        messageTable.addMessage(message.getMessageID(),"1",message);
+        messageTable.addMessage(message2.getMessageID(),"1",message2);
+        sensor.putVariable(mtVar,messageTable.serialize());
+
+        Command_ISINMT isinmt = new Command_ISINMT(sensor,mtVar,message2.serialize(),output);
+        isinmt.execute();
+        assertEquals("true",sensor.getVariableValue("$"+output));
+    }
+
+    @Test
+    void CheckInEmpty(){
+        TestableSensorNode sensor = new TestableSensorNode(2);
+        String mtvar = "mt";
+        String output = "x";
+        MessageTable messageTable = new MessageTable();
+        sensor.putVariable(mtvar,messageTable.serialize());
+        DataPackage message = new DataPackage("12","1","Hello world");
+
+        Command_ISINMT isinmt = new Command_ISINMT(sensor,mtvar, message.serialize(),output);
+        isinmt.execute();
+
+        assertEquals("false",sensor.getVariableValue("$"+output));
     }
 
 }
